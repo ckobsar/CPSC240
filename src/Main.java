83cd59ca259bdb3f0ae74e4538ae63cfab958ad6
jackstaa <jackc891@gmail.com>
2022-11-29 13:50:38 -0500

    Made main Panel, Added Labels and buttons also added scrollpane

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
     }
 }
--- src/Quiz.java
+++ src/Quiz.java
@@ -1,10 +1,31 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Quiz {
+public class Quiz extends JFrame implements ActionListener {
     private String fileName;
-    private ArrayList<State> quiz;
+    private static ArrayList<State> quiz;
+    static JLabel counterLabel = new JLabel("\tCorrect: ");
+    static int counter = 0;
+    static JPanel mainPanel = new JPanel();
+    static JPanel stateContainer = new JPanel();
+    static JLabel questionLabel = new JLabel("What is the Capitol of: " + quiz.get(0).getStateName());
+
+    // 50 state buttons in an array
+    static JButton[] states = new JButton[50];
+
+    // here are the four choices that each state has
+    static JButton choice1 = new JButton(quiz.get(0).getChoice1());
+    static JButton choice2 = new JButton(quiz.get(0).getChoice2());
+    static JButton choice3 = new JButton(quiz.get(0).getChoice3());
+    static JButton choice4 = new JButton(quiz.get(0).getChoice4());
+
+    // scroll the 50 states horizontally
+    static JScrollPane scrollPane = new JScrollPane(stateContainer);
+
 
     public Quiz(String fileName){
         File file = new File("stateslist.txt");
@@ -20,6 +41,17 @@ public class Quiz {
         catch (Exception stateReadError){
             stateReadError.printStackTrace();
         }
+    }
+    public double userResults(){
+        return 0.0;
+    }
+    public void userScore(){
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
     }
 }
 
